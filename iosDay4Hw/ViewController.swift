9644bc2f58ec26204ace9edda3bed36a308b9f55
Tarlochan <brotherstarlochan5268@gmail.com>
2019-07-04 15:20:49 -0400

    Initial Commit

---
+++ iosDay4Hw/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosDay4Hw
+//
+//  Created by admin on 2019-07-04.
+//  Copyright © 2019 Tarlochan5268. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
